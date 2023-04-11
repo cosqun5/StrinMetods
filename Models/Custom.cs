2aff3b8a16f46c384d2ce5cd88b38168c1ec802f
cosqun5 <87088111+cosqun5@users.noreply.github.com>
2023-04-11 13:29:33 +0400

    first commit

---
+++ Models/Custom.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_methods.Models
+{
+    public class Custom
+    {
+        public bool Start(string name, char chars)
+        {
+            if (name[0] == chars)
+            {
+                return true;
+            }
+            return false;
+      
+
+        }
+        public bool End(string name , char chars)
+        {
+            if (name[name.Length-1] == chars)
+            {
+                return true;
+            }
+            return false;
+
+
+
+        }
+        public bool Contain(string name, char chars)
+        {
+            for (int i = 0; i < name.Length; i++)
+            {
+              if (name[i]== chars)
+                {
+                    return true;
+                }
+            
+            }
+            return false;
+        }
+
+        public int Index(string name, char chars)
+        {
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (name[i] == chars)
+                {
+                    return i;
+                }
+            }
+            return -1;
+
+        }
+
+        public int LastIndex(string name, char chars)
+        {
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                if (name[name.Length - 1]==chars)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        
+        public string Replace(string Name, char oldchar,char newchar)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < Name.Length; i++)
+            {
+                if (Name[i]==oldchar)
+                {
+                    sb.Append(newchar);
+                     
+                }
+                else
+                {
+                    sb.Append(Name[i]);
+                }
+            }
+                return sb.ToString();
+        }
+        public string Remove(string Name ,char num)
+        {
+            StringBuilder  sb = new StringBuilder();
+            for (int i = 0; i < Name.Length; i++)
+            {
+                if (Name[i]==num)
+                {
+                    sb.Remove(2,2);
+                }
+                else
+                {
+                    sb.Append(Name[i]);
+                }
+            }
+            return sb.ToString();
+        }
+        public string Trim(string Name)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < Name.Length; i++)
+            {
+                if (Name[i] !=' ')
+                {
+
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using String_methods.Models;
+using System.Text;
+
+Custom custom= new Custom();
+//Console.WriteLine(custom.Start("Hello", 'P'));
+//Console.WriteLine(custom.End("Hello ", ' '));
+//Console.WriteLine(custom.Contain("Hello", 'e'));
+//Console.WriteLine(custom.Index("Hello", 'o'));
+//Console.WriteLine(custom.LastIndex("Hello", 'o'));
+
+
+ //Console.WriteLine(custom.Replace("Hello", 'l','a'));
+
+Console.WriteLine(custom.Remove("Hello", 'H'));
+
+
+
+
+
+
+
